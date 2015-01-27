768f90fd3e55c947bbe660877c8eb6de13a30aaa
Tim Martin <timothy.m.martin@gmail.com>
2015-01-27 00:16:47 -0800

    Updated navbar and moved footer to components.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('prosperenceApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+    $scope.menu = [
+      { 'title': 'Home', 'link': '/' },
+      { 'title': 'About', 'link': '/about' },
+      { 'title': 'Resources', 'link': '/resources' },
+      { 'title': 'Start My Plan', 'link': '/plan-builder' }
+    ];
 
     $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
 
     $scope.isActive = function(route) {
       return route === $location.path();
