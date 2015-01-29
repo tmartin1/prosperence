d0e40a320f3100ed6523c451fc8d412a7fd3c1bc
Tim Martin <timothy.m.martin@gmail.com>
2015-01-29 11:37:31 -0800

    Modularized the navbar within the plan builder and gave it a progress bar.

--- client/components/builderNav/builderNav.controller.js
+++ client/components/builderNav/builderNav.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('prosperenceApp')
-  .controller('BuilderNavCtrl', function ($scope, $location, Auth) {
+  .controller('BuilderNavCtrl', function($scope, $location, Auth) {
     // Defines the order of how pages are displayed to the user.
     var order = [
       '/nws',
@@ -10,10 +10,10 @@ angular.module('prosperenceApp')
       '/risk',
       '/retire'
     ];
-    var current = order.indexOf($location.path());
-    $scope.previous = order[current-1] || false;
-    $scope.next = order[current+1] || false;
-    $scope.progress = (current / order.length) * 100 + '%';
+    var current = Math.max(0, order.indexOf($location.path()));
+    $scope.previous = order[current - 1] || false;
+    $scope.next = order[current + 1] || false;
+    $scope.progress = Math.max(.05, (current / order.length)) * 100 + '%';
 
     $scope.isCollapsed = true;
     $scope.getCurrentUser = Auth.getCurrentUser;
