6e0e8a90a7c4bc726b06f9a016bc585e4e008ccf
Tim Martin <timothy.m.martin@gmail.com>
2015-01-31 20:05:04 -0800

    Updated index, university.html, and routes for new univeristy states.

--- client/app/university/university.js
+++ client/app/university/university.js
@@ -1,11 +1,34 @@
 'use strict';
 
 angular.module('prosperenceApp')
-  .config(function($stateProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/university/welcome");
+
     $stateProvider
       .state('university', {
         url: '/university',
         templateUrl: 'app/university/university.html',
-        controller: 'UniversityCtrl'
+        controller: 'UniversityCtrl',
+        abstract: true
+      })
+      .state('university.courses', {
+        url: '/courses',
+        templateUrl: 'app/university/courses/courses.html',
+        controller: 'CoursesCtrl'
+      })
+      .state('university.paths', {
+        url: '/paths',
+        templateUrl: 'app/university/paths/paths.html',
+        controller: 'PathsCtrl'
+      })
+      .state('university.resources', {
+        url: '/resources',
+        templateUrl: 'app/university/resources/resources.html',
+        controller: 'ResourcesCtrl'
+      })
+      .state('university.welcome', {
+        url: '/welcome',
+        templateUrl: 'app/university/welcome/welcome.html',
+        controller: 'WelcomeCtrl'
       });
   });
