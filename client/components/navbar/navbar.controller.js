4a2d8756c234aaf987f97aa7c40a542bd03bbd92
Tim Martin <timothy.m.martin@gmail.com>
2015-02-03 11:23:31 -0800

    Dynamically shows 'My Plan' or 'Start Planning' depending on login status.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,25 +2,34 @@
 
 angular.module('prosperenceApp')
   .controller('NavbarCtrl', function($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }, {
-      'title': 'About',
-      'link': '/about'
-    }, {
-      'title': 'My Plan',
-      'link': '/plan-builder'
-    }, {
-      'title': 'University of Prosperence',
-      'class': 'fa fa-graduation-cap',
-      'link': '/university'
-    }, ];
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/',
+      'shown': true
+    }, {
+      'title': 'About',
+      'link': '/about',
+      'shown': true
+    }, {
+      'title': 'My Plan',
+      'link': '/my-plan',
+      'shown': 'isLoggedIn()'
+    }, {
+      'title': 'Start Planning',
+      'link': '/plan-builder',
+      'shown': '!isLoggedIn()'
+    }, {
+      'title': 'University of Prosperence',
+      'class': 'fa fa-graduation-cap',
+      'link': '/university',
+      'shown': true
+    }, ];
 
     $scope.logout = function() {
       Auth.logout();
