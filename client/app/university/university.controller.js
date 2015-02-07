21c07ae87f8b842d6a9a3d3db77dffeb9b4d3c61
Tim Martin <timothy.m.martin@gmail.com>
2015-02-06 17:53:42 -0800

    Fixed highlighting active links on university navbar.

--- client/app/university/university.controller.js
+++ client/app/university/university.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('prosperenceApp')
-  .controller('UniversityCtrl', function($scope) {
-    $scope.message = 'Hello';
+  .controller('UniversityCtrl', function($scope, $state) {
+
+    // Sets active class on sidebar.
+    $scope.isActive = function(viewLocation) {
+      return viewLocation === $state.current.url;
+    };
+
   });
