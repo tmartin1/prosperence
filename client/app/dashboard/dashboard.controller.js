8c62f1eacad89568293b4f893985f96c5fae4dc0
Tim Martin <timothy.m.martin@gmail.com>
2015-03-01 15:25:21 -0800

    Fix menu view, active view, and default settings for nested state views.

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -2,6 +2,9 @@
 
 angular.module('prosperenceApp')
 .controller('DashboardCtrl', function ($scope, $state) {
+  // Defines initial view conditions for my-plan and settings.
+  $scope.myPlanView = $scope.myPlanView || getPath('my-plan/nws/nws.html');
+  $scope.settingsView = $scope.settingsView || getPath('settings/basic/basic.html');
 
   // Sidebar information.
   $scope.sidebar = [{
@@ -12,36 +15,58 @@ angular.module('prosperenceApp')
     title: 'My Plan',
     link: 'dashboard.my-plan',
     icon: '',
+    setView: 'myPlanView',
     submenu: [{
       title: 'Net Worth',
-      view: getPath('/nws/nws.html'),
+      view: getPath('my-plan/nws/nws.html'),
       active: true
     }, {
       title: 'Budget',
-      view: getPath('/msa/msa.html'),
+      view: getPath('my-plan/msa/msa.html'),
       active: false
     }, {
       title: 'Insurance',
-      view: getPath('/insurance/insurance.html'),
+      view: getPath('my-plan/insurance/insurance.html'),
       active: false
     }, {
       title: 'Retirement',
-      view: getPath('/retire/retire.html'),
+      view: getPath('my-plan/retire/retire.html'),
       active: false
     }, {
       title: 'Add More',
-      view: getPath('/more/more.html'),
+      view: getPath('my-plan/more/more.html'),
       active: false
     }]
   }, {
     title: 'Settings',
     link: 'dashboard.settings',
-    icon: 'glyphicon glyphicon-cog'
+    icon: 'glyphicon glyphicon-cog',
+    setView: 'settingsView',
+    submenu: [{
+      title: 'Basic',
+      view: getPath('settings/basic/basic.html'),
+      active: true
+    }, {
+      title: 'Notifications',
+      view: getPath('settings/notifications/notifications.html'),
+      active: false
+    }, {
+      title: 'Security',
+      view: getPath('settings/security/security.html'),
+      active: false
+    }]
   }];
+  
+  // Check and set submenu viewability on load/state enter.
+  for (var i=0; i<$scope.sidebar.length; i++) {
+    if ($state.current.name === $scope.sidebar[i].link && $scope.sidebar[i].submenu) {
+      $scope.sidebar[i].submenu.visible = true;
+    }
+  }
 
-  // Returns the full path of the input within ./dashboard/my-plan.
+  // Returns the full path of the input within ./dashboard/.
   function getPath(localPath) {
-    return 'app/dashboard/my-plan' + localPath;
+    return 'app/dashboard/' + localPath;
   }
 
   // Sets visibility of submenus in the sidebar.
@@ -53,31 +78,18 @@ angular.module('prosperenceApp')
       }
     }
     // Set selected sidebar submenu to visible.
-    if (selected.submenu) {
+    if (!!selected && selected.submenu) {
       selected.submenu.visible = true;
     }
   };
 
-  // Check and set submenu viewability on load/state enter.
-  for (var i=0; i<$scope.sidebar.length; i++) {
-    if ($state.current.name === $scope.sidebar[i].link && $scope.sidebar[i].submenu) {
-      $scope.sidebar[i].submenu.visible = true;
-    }
-  }
-
   // Sets the current view for my-plan.
-  $scope.setPlanView = function(sub, submenu) {
-    if (!sub) {
-      getPath('/nws/nws.html');
-    } else {
-      for (var i=0; i<submenu.length; i++) {
-        submenu[i].active = false;
-      }
-      $scope.myPlanView = sub.view;
-      sub.active = true;
+  $scope.setSubView = function(sub, submenu, section) {
+    for (var i=0; i<submenu.length; i++) {
+      submenu[i].active = false;
     }
+    sub.active = true;
+    $scope[section.setView] = sub.view;
   };
-  $scope.setPlanView();
-
 
 });
