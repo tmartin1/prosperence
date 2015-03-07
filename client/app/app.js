3e24817d5d9ccab3f2ebfdad09c776ac3e1004ad
Elliott Thoreby <ethoreby@gmail.com>
2015-03-07 09:35:49 -0800

    rm login signup routes

--- client/app/app.js
+++ client/app/app.js
@@ -28,7 +28,7 @@ angular.module('prosperenceApp', [
     // Intercept 401s and redirect you to login
     responseError: function(response) {
       if (response.status === 401) {
-        $location.path('/login');
+        $location.path('/');
         // remove any stale tokens
         $cookieStore.remove('token');
         return $q.reject(response);
@@ -43,7 +43,7 @@ angular.module('prosperenceApp', [
   $rootScope.$on('$stateChangeStart', function(event, next) {
     Auth.isLoggedInAsync(function(loggedIn) {
       if (next.authenticate && !loggedIn) {
-        $location.path('/login');
+        $location.path('/');
       }
     });
   });
