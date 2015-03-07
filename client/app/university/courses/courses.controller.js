f8898892583f292e73ae929f651769c163dcc679
Scott Rice <scottrice10@gmail.com>
2015-03-06 20:48:28 -0800

    Begin to add search ui to Course section

--- client/app/university/courses/courses.controller.js
+++ client/app/university/courses/courses.controller.js
@@ -1,6 +1,100 @@
 'use strict';
 
 angular.module('prosperenceApp')
-.controller('CoursesCtrl', function($scope) {
-  $scope.message = 'Hello';
-});
+  .controller('CatalogCtrl', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
+    $scope.facetFields = "";
+    $scope.filterFields = "";
+    $scope.selectedItems = [];
+    $scope.from = 0;
+    $scope.page = 1;
+    $scope.prevPage = 1;
+    $scope.noOfSuggests = 5;
+    $scope.checked = [];
+    $scope.filterFields = [];
+    $scope.searchInProgress = true;
+    $scope.clickLimit = 5;
+    $scope.showMoreFacets = true;
+    $scope.startDurationFilter = 0;
+    $scope.endDurationFilter = 0;
+
+    // Search by facet filter.
+    $scope.doSearchByFilter = function (term, value) {
+      $scope.checked[value] = !$scope.checked[value];
+
+      if ($scope.checked[value]) {
+        $scope.filterFields.push({
+          term: term,
+          value: value
+        });
+      } else {
+        $scope.filterFields.forEach(function (filter, i) {
+          if (filter.value === value) {
+            $scope.filterFields.splice(i, 1);
+          }
+        })
+      }
+
+      $scope.doSearch($scope.searchTerm, 0, $scope.filterFields);
+    };
+
+    $scope.doSearch = function (searchTerm, pageNumber, filterFields) {
+      pageNumber = pageNumber || 0;
+      filterFields = filterFields || null;
+      $scope.searchTerm = searchTerm;
+      search.doSearch(searchTerm, pageNumber, filterFields, null, function (newProducts) {
+        search.processFacets(newProducts);
+      });
+    };
+
+    // Function for fetch page results.
+    $scope.fetchPage = function (searchTerm, pageNumber) {
+      pageNumber = (pageNumber - 1) * 12;
+      $scope.doSearch(searchTerm, pageNumber);
+    };
+
+    // Function to sort by duration
+    $scope.doDurationSort = function () {
+      //remove existing duration filter, if exists
+      $scope.filterFields.forEach(function (filter, i) {
+        if (filter.term === 'duration') {
+          $scope.filterFields.splice(i, 1);
+        }
+      });
+
+      // filter by duration range
+      $scope.doSearchByFilter('duration', '[' + $scope.startDurationFilter + ',' + $scope.endDurationFilter + ']');
+    };
+
+    //ajax call to show more favorite records
+    $scope.showMoreFacetLinks = function (numberToShow) {
+      // toggle to "Show Less"
+      $scope.showMoreFacets = false;
+
+      // set limit filter for ng-repeat
+      $scope.clickLimit = numberToShow;
+    };
+
+    //ajax call to show less favorite records
+    $scope.showLessFacetLinks = function () {
+      // toggle to "Show More"
+      $scope.showMoreFacets = true;
+
+      // set limit filter for ng-repeat
+      $scope.clickLimit = 5;
+    };
+
+    //INIT
+    //initially, if products empty, then call search to show items
+    if (!search.fromNavBar) {
+      $scope.doSearch('', 0, function (newProducts) {
+        $scope.products = newProducts;
+      });
+    }
+
+    $scope.$on('search-in-progress', function (event, args) {
+      $scope.products.results = [];
+      $scope.products.totalCount = 0;
+      $scope.searchInProgress = true;
+    })
+  }]);
+
--- client/app/university/university.controller.js
+++ client/app/university/university.controller.js
@@ -1,6 +1,34 @@
 'use strict';
 
 angular.module('prosperenceApp')
-.controller('UniversityCtrl', function($scope, $state) {
-  // 
-});
+.controller('UniversityCtrl', function($scope, search) {
+    $scope.doSearch = function(searchTerm, pageNumber) {
+      pageNumber = pageNumber || 0;
+      $scope.searchTerm = searchTerm;
+
+      search.doSearch(searchTerm, pageNumber, null, null, function(newProducts) {
+        newProducts = search.processFacets(newProducts);
+        $rootScope.$broadcast('products-updated', {
+          newProducts: newProducts
+        });
+        search.fromNavBar = true;
+      });
+    };
+
+    $scope.doSuggestor = function(searchTerm) {
+      $scope.searchTerm = searchTerm;
+      search.doSuggestor(searchTerm, function(newProducts) {
+        $scope.suggestedProducts = newProducts;
+      });
+    };
+
+    //when enter pressed, trigger search if no suggestions given
+    $rootScope.$on('keypress', function(onEvent, keypressEvent) {
+      var keyCode = keypressEvent.which;
+
+      if (keyCode === 13 && $scope.searchTerm) {
+        $scope.doSearch($scope.searchTerm);
+      }
+    });
+
+  });
