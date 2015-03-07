66db36b8147407eb4d3d22ac82e56809a5c1e61c
Tim Martin <timothy.m.martin@gmail.com>
2015-03-07 15:07:45 -0800

    Update plan builder style.

--- client/app/plan-builder/debts/debts.spec.js
+++ client/app/plan-builder/debts/debts.spec.js
@@ -1,21 +1,21 @@
-'use strict';
-
-describe('Controller: DebtsCtrl', function () {
-
-  // load the controller's module
-  beforeEach(module('prosperenceApp'));
-
-  var DebtsCtrl, scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    DebtsCtrl = $controller('DebtsCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should ...', function () {
-    expect(1).toEqual(1);
-  });
-});
+// 'use strict';
+//
+// describe('Controller: DebtsCtrl', function () {
+//
+//   // load the controller's module
+//   beforeEach(module('prosperenceApp'));
+//
+//   var DebtsCtrl, scope;
+//
+//   // Initialize the controller and a mock scope
+//   beforeEach(inject(function ($controller, $rootScope) {
+//     scope = $rootScope.$new();
+//     DebtsCtrl = $controller('DebtsCtrl', {
+//       $scope: scope
+//     });
+//   }));
+//
+//   it('should ...', function () {
+//     expect(1).toEqual(1);
+//   });
+// });
