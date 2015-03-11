30e732b29aa2c4caf2d0d5a03a3cdcfd1a5a386b
Tim Martin <timothy.m.martin@gmail.com>
2015-03-11 13:44:30 -0700

    Finish refactoring all sections of 'my-plan ' to planels.

--- client/app/dashboard/my-plan/more/more.controller.js
+++ client/app/dashboard/my-plan/more/more.controller.js
@@ -1,6 +1,9 @@
+// NOTE: While the other views of My-Plan are comprised of planels, this section should not be. Instead, this section
+// should contain (artistic, beautiful, etc.) links to other non-core plan items (college savings, student loans, etc.)
+
 'use strict';
 
 angular.module('prosperenceApp')
-.controller('MoreCtrl', function ($scope) {
+.controller('MyPlanMoreCtrl', function ($scope) {
   //
 });
