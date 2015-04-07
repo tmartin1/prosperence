bd506c0fd871a927c10893daa1120dff052cdb7f
Tim Martin <timothy.m.martin@gmail.com>
2015-04-06 17:24:15 -0700

    Update db seed data with advisor and question examples.

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,9 +7,16 @@
 
 var User = require('../api/user/user.model');
 var testUser = require('../../tests/testUser');
+var Question = require('../api/question/question.model');
 
 User.find({}).remove(function() {
-  User.create(testUser,
+  User.create(testUser, {
+    provider: 'local',
+    name: 'Test Advisor',
+    email: 'advisor@test.com',
+    password: 'advisor',
+    isAdvisor: true
+  },
     // {
     //   provider: 'local',
     //   role: 'admin',
@@ -18,7 +25,67 @@ User.find({}).remove(function() {
     //   password: 'admin'
     // },
     function() {
-      console.log('finished populating users');
+      console.log('Finished populating users.');
     }
   );
+});
+
+Question.find({}).remove(function() {
+  Question.create({
+    text: 'When are ETFs better than mutual funds?',
+    author: 'Test User',
+    rating: 47,
+    responses: [{
+      name: 'Example Advisor',
+      text: 'Great question, [insert appropriate, well written, unbiased response here].',
+      isAdvisor: true
+    }]
+  }, {
+    text: 'Should I contribute to my Roth 401k or Traditional 401k?',
+    author: 'Test User',
+    rating: 18,
+    responses: [{
+      name: 'Example Advisor',
+      text: 'Great question, [insert appropriate, well written, unbiased response here].',
+      isAdvisor: true
+    }]
+  }, {
+    text: 'Is whole life insurance a scam?',
+    author: 'Test User',
+    rating: 12345,
+    responses: [{
+      name: 'Example Advisor',
+      text: 'Great question, [insert appropriate, well written, unbiased response here].',
+      isAdvisor: true
+    }]
+  }, {
+    text: 'Can a municipal bond fund be used as an emergency reserve?',
+    author: 'Test User',
+    rating: 77,
+    responses: [{
+      name: 'Example Advisor',
+      text: 'Great question, [insert appropriate, well written, unbiased response here].',
+      isAdvisor: true
+    }]
+  }, {
+    text: 'How can I protect my estate from potential lawsuites?',
+    author: 'Example User',
+    rating: 4,
+    responses: [{
+      name: 'Example Advisor',
+      text: 'Great question, [insert appropriate, well written, unbiased response here].',
+      isAdvisor: true
+    }]
+  }, {
+    text: 'How should I balance paying off my debts while still trying to save for retirement?',
+    author: 'Example User',
+    rating: 317,
+    responses: [{
+      name: 'Example Advisor',
+      text: 'Great question, [insert appropriate, well written, unbiased response here].',
+      isAdvisor: true
+    }]
+  }, function() {
+    console.log('Finished populating questions.');
+  });
 });
