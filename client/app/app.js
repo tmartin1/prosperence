6dadea71caea1d59e1638362ce001cbbe4af3b8a
Tim Martin <timothy.m.martin@gmail.com>
2015-04-06 17:23:34 -0700

    Add socket dependencies to app.js.

--- client/app/app.js
+++ client/app/app.js
@@ -10,18 +10,18 @@ angular.module('prosperenceApp', [
   'prosperenceApp.filter',
   'angularUtils.directives.dirPagination',
   'LocalStorageModule',
-  'youtube-embed'
+  'youtube-embed',
+  'btford.socket-io'
 ])
 .config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-  $urlRouterProvider
-    .otherwise('/');
+  $urlRouterProvider.otherwise('/');
 
   $locationProvider.html5Mode(true);
   $httpProvider.interceptors.push('authInterceptor');
 })
 .factory('authInterceptor', function($rootScope, $q, $cookieStore, $location) {
   return {
-    // Add authorization token to headers
+    // Add authorization token to headers.
     request: function(config) {
       config.headers = config.headers || {};
       if ($cookieStore.get('token')) {
@@ -30,11 +30,11 @@ angular.module('prosperenceApp', [
       return config;
     },
 
-    // Intercept 401s and redirect you to login
+    // Intercept 401s and redirect you to login.
     responseError: function(response) {
       if (response.status === 401) {
         $location.path('/');
-        // remove any stale tokens
+        // Remove any stale tokens.
         $cookieStore.remove('token');
         return $q.reject(response);
       } else {
@@ -44,7 +44,7 @@ angular.module('prosperenceApp', [
   };
 })
 .run(function($rootScope, $location, Auth) {
-  // Redirect to login if route requires auth and you're not logged in
+  // Redirect to login if route requires auth and you're not logged in.
   $rootScope.$on('$stateChangeStart', function(event, next) {
     Auth.isLoggedInAsync(function(loggedIn) {
       if (next.authenticate && !loggedIn) {
