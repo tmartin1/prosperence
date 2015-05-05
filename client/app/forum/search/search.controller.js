9a952b81b878fef7d631e64d3d63269b64c75213
Tim Martin <timothy.m.martin@gmail.com>
2015-05-04 18:47:07 -0700

    Set min question length to 4 words, no all caps.

--- client/app/forum/search/search.controller.js
+++ client/app/forum/search/search.controller.js
@@ -14,15 +14,26 @@ angular.module('prosperenceApp')
     if (!Auth.isLoggedIn()) return $rootScope.openLoginModal();
     // Only non-advisors can post questions.
     if (!Auth.isAdvisor()) {
-      if($scope.newQuestion !== '') {
+      if ($scope.newQuestion.split(' ').length < 4) {
+        // Require questions to be at least 4 words long.
+        alert('Questions that are more specific are more likely to get better responses. Please be a little more specific with your question.')
+      } else if (!!allCaps($scope.newQuestion)) {
+        // Require questions to NOT be in all caps.
+        alert('Please avoid using all caps when asking questions.');
+      } else {
         $http.post('/api/questions', {
           text: $scope.newQuestion,
           author: Auth.getCurrentUser().name,
           timestamp: new Date()
         });
+        $scope.newQuestion = '';
       }
-      $scope.newQuestion = '';
     }
+  };
+
+  // Returns true if string is all caps, false otherwise.
+  function allCaps(str) {
+    return str.toUpperCase() === str;
   };
 
 });
